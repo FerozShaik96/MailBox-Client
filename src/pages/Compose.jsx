af395ff134d90cf6bfa072cab0754ee2f83b7e3f
FerozShaik96 <ferozshaik1997@gmail.com>
2023-08-29 13:12:16 +0530

    Changed the logic to storing the mail to data inthe form of different sender and Reciver

--- src/pages/Compose.jsx
+++ src/pages/Compose.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { EditorState } from 'draft-js';
 import React, { useState } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -11,10 +12,11 @@ function Compose() {
   const navigate = useNavigate();
   const [recipients, setRecipients] = useState('');
   const [subject, setSubject] = useState('');
-  const [description, setDescrtiption] = useState('');
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty(),
+  );
   const localId = localStorage.getItem('LocalId');
   const emailid = localStorage.getItem('emailId');
-  console.log(emailid);
   const recipientsHandler = (event) => {
     setRecipients(event.target.value);
   };
@@ -22,52 +24,57 @@ function Compose() {
     setSubject(event.target.value);
   };
   const onEditorStateChange = (event) => {
-    setDescrtiption(event.blocks[0].text);
+    setEditorState(event);
   };
+  const reciver = recipients.replace(/@.*/, '');
   const EmailReplace = emailid.replace(/@.*/, '');
-  console.log(EmailReplace);
   const submitHandler = async (event) => {
     event.preventDefault();
     const seen = false;
+    const isDeleted = false;
     const data = {
       recipients,
       subject,
-      description,
+      description: editorState.getCurrentContent().getPlainText(),
       seen,
+      isDeleted,
     };
-    console.log(description);
-    const composeData = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentmails/${localId}.json`,
-      {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      const composeData = await fetch(
+        `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentBox/${EmailReplace}.json`,
+        {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    );
-    if (composeData.ok) {
-      const composeResponse = await composeData;
-      console.log(composeResponse);
-    }
-    const recivedmail = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/recivedmails/${EmailReplace}.json`,
-      {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
+      );
+      if (composeData.ok) {
+        const composeResponse = await composeData;
+        console.log(composeResponse);
+      }
+      const recivedmail = await fetch(
+        `https://mail-box-client-1328c-default-rtdb.firebaseio.com/inbox/${reciver}.json`,
+        {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    );
-    if (recivedmail.ok) {
-      const recivedRes = await recivedmail;
-      console.log(recivedRes);
+      );
+      if (recivedmail.ok) {
+        const recivedRes = await recivedmail;
+        console.log(recivedRes);
+      }
+      setEditorState('');
+      setRecipients('');
+      setSubject('');
+      navigate('/login');
+    } catch (err) {
+      alert(err.message);
     }
-    setDescrtiption('');
-    setRecipients('');
-    setSubject('');
-    navigate('/login');
   };
 
   return (
@@ -75,37 +82,37 @@ function Compose() {
       <div className=" flex items-center justify-between border-b border-gray-700 border-opacity-30 ps-2 hover:shadow-lg">
         <div className="ms-2 flex h-14 w-2/12 items-center justify-evenly  ">
           <div className="flex  ">
-            <button className="rounded-lg px-1 py-2 hover:bg-slate-300">
+            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
               <RxBox className="text-xl " />
             </button>
-            <button className="rounded-lg px-1 py-2  text-gray-600 hover:bg-slate-300">
-              <TiArrowSortedDown className="text-md text-gray-600" />
+            <button className="rounded-lg px-1 py-2  text-gray-700  hover:bg-slate-500 hover:bg-opacity-30">
+              <TiArrowSortedDown className="text-md" />
             </button>
           </div>
-          <button className="rounded-full px-3 py-3 text-gray-600 hover:bg-slate-300 hover:duration-700">
+          <button className="rounded-full px-3 py-3 text-gray-700  hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
             <TbRotate />
           </button>
-          <button className="rounded-full px-3 py-3 text-gray-600 hover:bg-slate-300 hover:duration-700">
+          <button className="rounded-full px-3 py-3 text-gray-700  hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
             <TbDotsVertical />
           </button>
         </div>
-        <div className="flex h-12 items-center gap-x-2  text-gray-500">
-          <button className=" rounded-md px-1 py-2 text-sm hover:bg-gray-200">
+        <div className="flex h-12 items-center gap-x-2  text-gray-900">
+          <button className=" rounded-md px-1 py-2 text-sm hover:bg-slate-500 hover:bg-opacity-30">
             1-50 of 327
           </button>
           <div className="flex gap-x-4">
-            <button className="rounded-full px-3 py-3 duration-700 hover:bg-slate-300">
+            <button className="rounded-full px-3 py-3 duration-700  hover:bg-slate-500 hover:bg-opacity-30">
               <LiaAngleLeftSolid className=" text-md" />
             </button>
-            <button className="rounded-full px-3 py-3 duration-700 hover:bg-slate-300">
+            <button className="rounded-full px-3 py-3 duration-700  hover:bg-slate-500 hover:bg-opacity-30">
               <LiaAngleRightSolid className="text-md" />
             </button>
           </div>
           <div className="me-4 flex">
-            <button className="rounded-lg px-1 py-2 hover:bg-slate-300">
+            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
               <BsKeyboardFill className=" text-md  " />
             </button>
-            <button className="rounded-lg px-1 py-2 hover:bg-slate-300">
+            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
               <TiArrowSortedDown className=" text-md  " />
             </button>
           </div>
@@ -115,7 +122,7 @@ function Compose() {
       <div className="shadow-3xl mx-auto my-auto   w-1/2   rounded-t-md   text-center">
         {/* absolute right-14 top-24 */}
         <div className=" shadow-3xl h-screen rounded-lg border border-gray-500 border-opacity-25">
-          <h1 className=" rounded-t-lg bg-slate-400 bg-opacity-30">
+          <h1 className=" rounded-t-lg bg-slate-400 bg-opacity-30 py-2">
             New Message
           </h1>
           <form onSubmit={submitHandler} className=" h-5/6">
@@ -135,14 +142,14 @@ function Compose() {
               onChange={subjectHandler}
               className="w-full  border py-2 ps-3 shadow-sm  focus:outline-none "
             />
-            <div className="h-4/6  ">
+            <div className="h-4/6 rounded-b-xl bg-white  ">
               <Editor
                 type="text"
-                value={description}
-                onChange={onEditorStateChange}
+                editorState={editorState}
+                onEditorStateChange={onEditorStateChange}
               />
             </div>
-            <div className="ms-10 text-left">
+            <div className="ms-10 mt-4 text-left">
               <button
                 type="submit"
                 className="  my-2 rounded-full border border-blue-700 bg-blue-700 px-10 py-2"
