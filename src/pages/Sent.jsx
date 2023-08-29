4a58bbd0a19b33f9953f26e2ea7215d11eacdba1
FerozShaik96 <ferozshaik1997@gmail.com>
2023-08-29 21:32:22 +0530

    Added Redux to the Sent Page

--- src/pages/Sent.jsx
+++ src/pages/Sent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { RxBox, RxClock } from 'react-icons/rx';
 import { TiArrowSortedDown } from 'react-icons/ti';
 import { LiaAngleLeftSolid, LiaAngleRightSolid } from 'react-icons/lia';
@@ -7,35 +8,45 @@ import { TbStar, TbRotate, TbDotsVertical, TbTrash } from 'react-icons/tb';
 import { MdLabelImportantOutline, MdOutlineEmail } from 'react-icons/md';
 import { BiArchiveIn } from 'react-icons/bi';
 import { BsKeyboardFill } from 'react-icons/bs';
+import { SendActions } from '../store/SendReducer';
 const Sent = () => {
   const [sentData, setSentData] = useState([]);
   const email = localStorage.getItem('emailId').replace(/@.*/, '');
+  const dispatch = useDispatch();
+  const sentDataReducer = useSelector((state) => state.sendRedcuer.sendBox);
+  console.log(sentDataReducer);
   const fetchData = async () => {
     try {
       const data = await fetch(
         `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentBox/${email}.json`,
       );
-      const resData = await data.json();
-      const loadedData = [];
-      for (const key in resData) {
-        if (!resData[key].isDeleted) {
+      if (data.ok) {
+        const resData = await data.json();
+        const loadedData = [];
+        for (const key in resData) {
+          // if (!resData[key].isDeleted) {
           loadedData.push({
             id: key,
             subject: resData[key].subject,
             description: resData[key].description,
             recipitents: resData[key].recipients,
+            isDeleted: resData[key].isDeleted,
+            seen: resData[key].seen,
+            from: resData[key].EmailReplace,
             to: resData[key].recipients.split('@')[0],
           });
+          // }
         }
+        dispatch(SendActions.updateData(loadedData));
+        // setSentData(loadedData);
       }
-      setSentData(loadedData);
     } catch (err) {
       console.log(err);
     }
   };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [sentDataReducer]);
   const trashHandler = async (item) => {
     const delet = await fetch(
       `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentBox/${email}/${item.id}.json`,
@@ -92,43 +103,46 @@ const Sent = () => {
         </div>
       </div>
       <ul className="">
-        {sentData &&
-          sentData.map((item) => (
-            <li
-              key={item.id}
-              // className="group flex h-10 w-full items-center   border  border-gray-700 border-opacity-20  ps-8 text-gray-500 hover:text-gray-900 hover:shadow-md "
-            >
-              <Link
-                to={`${item.id}`}
-                className="group flex h-10 w-full items-center   border  border-gray-700 border-opacity-20  ps-8 text-gray-500 hover:text-gray-900 hover:shadow-md"
+        {sentDataReducer.map((item) => {
+          if (!item.isDeleted) {
+            return (
+              <li
+                key={item.id}
+                // className="group flex h-10 w-full items-center   border  border-gray-700 border-opacity-20  ps-8 text-gray-500 hover:text-gray-900 hover:shadow-md "
               >
-                <div className=" flex w-3/12 items-center gap-x-4 text-lg">
-                  <RxBox className="" />
-                  <TbStar className="" />
-                  <MdLabelImportantOutline className="text-xl" />
-                  <h1 className="flex">
-                    To: <span>{item.to}</span>
-                  </h1>
-                </div>
-                <div className="ms-3 flex w-8/12  gap-x-2 ">
-                  <p>{item.subject} -</p>
-                  <p>{item.description}</p>
-                </div>
+                <Link
+                  to={`${item.id}`}
+                  className="group flex h-10 w-full items-center   border  border-gray-700 border-opacity-20  ps-8 text-gray-500 hover:text-gray-900 hover:shadow-md"
+                >
+                  <div className=" flex w-3/12 items-center gap-x-4 text-lg">
+                    <RxBox className="" />
+                    <TbStar className="" />
+                    <MdLabelImportantOutline className="text-xl" />
+                    <h1 className="flex">
+                      To: <span>{item.to}</span>
+                    </h1>
+                  </div>
+                  <div className="ms-3 flex w-8/12  gap-x-2 ">
+                    <p>{item.subject} -</p>
+                    <p>{item.description}</p>
+                  </div>
 
-                <button className=" invisible me-4 flex items-center gap-x-4 text-lg hover:text-gray-900 group-hover:visible">
-                  <BiArchiveIn />
-                  <TbTrash
-                    onClick={(event) => {
-                      event.preventDefault();
-                      trashHandler(item);
-                    }}
-                  />
-                  <MdOutlineEmail />
-                  <RxClock />
-                </button>
-              </Link>
-            </li>
-          ))}
+                  <button className=" invisible me-4 flex items-center gap-x-4 text-lg hover:text-gray-900 group-hover:visible">
+                    <BiArchiveIn />
+                    <TbTrash
+                      onClick={(event) => {
+                        event.preventDefault();
+                        trashHandler(item);
+                      }}
+                    />
+                    <MdOutlineEmail />
+                    <RxClock />
+                  </button>
+                </Link>
+              </li>
+            );
+          }
+        })}
       </ul>
     </React.Fragment>
   );
