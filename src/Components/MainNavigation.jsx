f56639b978863af44f22598b0c02b9899d4ac29d
FerozShaik96 <ferozshaik1997@gmail.com>
2023-08-29 13:18:20 +0530

    removed the image

--- src/Components/MainNavigation.jsx
+++ src/Components/MainNavigation.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import NavSidebar from './NavSidebar';
 import Header from './Header';
-import imgg from '../utilities/imgg.jpg';
 import { Outlet } from 'react-router-dom';
 
 function MainNavigation() {
   return (
-    <div className=" relative h-full w-full">
-      <img
-        src={imgg}
-        alt=""
-        className=" absolute h-full w-full  object-fill mix-blend-overlay"
-      />
+    <div className="  relative h-full w-full bg-bgIMg bg-cover bg-no-repeat">
       <div className="flex  ">
         <NavSidebar />
         <div className="block w-full">
           <Header />
-          <div className="mx-auto mt-5 min-h-screen w-11/12 rounded-xl bg-white  ">
+          <div className="mx-auto mt-5  min-h-screen w-11/12 rounded-xl bg-gray-100 bg-opacity-90 ">
             <Outlet />
           </div>
         </div>
