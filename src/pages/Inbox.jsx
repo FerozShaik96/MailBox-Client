045492e82b7dfbf792b890e1bb4cfdb14602eb22
FerozShaik96 <ferozshaik1997@gmail.com>
2023-10-15 20:00:46 +0530

    Refactored Some the code

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,128 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { RxBox, RxClock } from 'react-icons/rx';
 import { useSelector, useDispatch } from 'react-redux';
-import { InboxActions } from '../store/InboxReducer';
 import { Link } from 'react-router-dom';
-import { TiArrowSortedDown } from 'react-icons/ti';
-import { TbStar, TbRotate, TbDotsVertical, TbTrash } from 'react-icons/tb';
-import { LiaAngleLeftSolid, LiaAngleRightSolid } from 'react-icons/lia';
+import { TbStar, TbTrash } from 'react-icons/tb';
 import { MdLabelImportantOutline, MdOutlineEmail } from 'react-icons/md';
 import { BiArchiveIn } from 'react-icons/bi';
-import { BsKeyboardFill } from 'react-icons/bs';
+import {
+  inboxAction,
+  trashChangeHandler,
+  InboxChanger,
+} from '../store/Actions/InboxAction';
+import ContentHeder from '../Components/ContentHeder';
 function Inbox() {
-  // const [reciverData, setReciveData] = useState([]);
-  const reciver = localStorage.getItem('emailId').split('@')[0];
   const dispatch = useDispatch();
   const inboxData = useSelector((state) => state.inboxReducer.inbox);
-  // console.log(inboxData);
-  const fetchData = async () => {
-    const inboxData = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/inbox/${reciver}.json`,
-    );
-    if (inboxData.ok) {
-      const resData = await inboxData.json();
-      const loadedData = [];
-      for (const key in resData) {
-        // if (!resData[key].isDeleted) {
-        loadedData.push({
-          id: key,
-          subject: resData[key].subject,
-          seen: resData[key].seen,
-          isDeleted: resData[key].isDeleted,
-          description: resData[key].description,
-          recipitents: resData[key].recipients,
-          from: resData[key].EmailReplace,
-        });
-        // }
-      }
-      dispatch(InboxActions.updateData(loadedData));
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [inboxData]);
   const trashHandler = async (item) => {
-    console.log('Hello');
-    console.log(item.id);
-    const trashData = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/inbox/${reciver}/${item.id}.json`,
-      {
-        method: 'PATCH',
-        body: JSON.stringify({
-          isDeleted: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-    if (trashData.ok) {
-      const dataTrash = await trashData.json();
-      dispatch(InboxActions.updateTrash(item.id));
-      console.log(dataTrash);
-    }
+    console.log(item);
+    dispatch(trashChangeHandler(item.id));
   };
+  useEffect(() => {
+    console.log('Hello');
+    const fetchData = async () => {
+      dispatch(inboxAction());
+    };
+    fetchData();
+  }, [trashHandler]);
   const InboxChangeHandler = async (item) => {
-    const inboxChange = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/inbox/${reciver}/${item.id}.json`,
-      {
-        method: 'PATCH',
-        body: JSON.stringify({
-          seen: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-    if (inboxChange) {
-      dispatch(InboxActions.updateSeen(item.id));
-      const data = await inboxChange.json();
-    }
+    dispatch(InboxChanger(item.id));
   };
   return (
     <React.Fragment>
-      <div className=" flex items-center justify-between border-b border-gray-700 border-opacity-30 ps-2 hover:shadow-lg">
-        <div className="ms-2 flex h-14 w-2/12 items-center justify-evenly  ">
-          <div className="flex  ">
-            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
-              <RxBox className="text-xl " />
-            </button>
-            <button className="rounded-lg px-1 py-2  text-gray-700 hover:bg-slate-500 hover:bg-opacity-30">
-              <TiArrowSortedDown className="text-md " />
-            </button>
-          </div>
-          <button className="rounded-full px-3 py-3 text-gray-700 hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
-            <TbRotate />
-          </button>
-          <button className="rounded-full px-3 py-3 text-gray-700 hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
-            <TbDotsVertical />
-          </button>
-        </div>
-        <div className="flex h-12 items-center gap-x-2  text-gray-700">
-          <button className=" rounded-md px-1 py-2 text-sm hover:bg-slate-500 hover:bg-opacity-30">
-            1-5 of 50
-          </button>
-          <div className="flex gap-x-4">
-            <button className="rounded-full px-3 py-3 duration-700 hover:bg-slate-500 hover:bg-opacity-30">
-              <LiaAngleLeftSolid className=" text-md" />
-            </button>
-            <button className="rounded-full px-3 py-3 duration-700 hover:bg-slate-500 hover:bg-opacity-30">
-              <LiaAngleRightSolid className="text-md" />
-            </button>
-          </div>
-          <div className="me-4 flex">
-            <button className="rounded-lg px-1 py-2 hover:bg-slate-500 hover:bg-opacity-30">
-              <BsKeyboardFill className=" text-md  " />
-            </button>
-            <button className="rounded-lg px-1 py-2 hover:bg-slate-500 hover:bg-opacity-30">
-              <TiArrowSortedDown className=" text-md  " />
-            </button>
-          </div>
-        </div>
-      </div>
-      <ul className="">
+      <ContentHeder />
+      <ul>
         {inboxData.map((item) => {
           if (!item.isDeleted) {
             return (
@@ -148,7 +57,7 @@ function Inbox() {
                     <p>{item.description}</p>
                   </div>
 
-                  <button className=" invisible me-4 flex items-center gap-x-4 text-lg hover:text-gray-700  group-hover:visible">
+                  <button className="  me-4 flex items-center gap-x-4 text-lg hover:text-gray-700  group-hover:visible">
                     <BiArchiveIn />
                     <TbTrash
                       onClick={(event) => {
--- src/pages/Sent.jsx
+++ src/pages/Sent.jsx
@@ -1,108 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { RxBox, RxClock } from 'react-icons/rx';
-import { TiArrowSortedDown } from 'react-icons/ti';
-import { LiaAngleLeftSolid, LiaAngleRightSolid } from 'react-icons/lia';
-import { TbStar, TbRotate, TbDotsVertical, TbTrash } from 'react-icons/tb';
+import { TbStar, TbTrash } from 'react-icons/tb';
 import { MdLabelImportantOutline, MdOutlineEmail } from 'react-icons/md';
 import { BiArchiveIn } from 'react-icons/bi';
-import { BsKeyboardFill } from 'react-icons/bs';
-import { SendActions } from '../store/SendReducer';
+import ContentHeder from '../Components/ContentHeder';
+import { sendBoxData, sentBoxTrash } from '../store/Actions/sentBoxActions';
 const Sent = () => {
-  const [sentData, setSentData] = useState([]);
-  const email = localStorage.getItem('emailId').replace(/@.*/, '');
   const dispatch = useDispatch();
   const sentDataReducer = useSelector((state) => state.sendRedcuer.sendBox);
   console.log(sentDataReducer);
   const fetchData = async () => {
-    try {
-      const data = await fetch(
-        `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentBox/${email}.json`,
-      );
-      if (data.ok) {
-        const resData = await data.json();
-        const loadedData = [];
-        for (const key in resData) {
-          // if (!resData[key].isDeleted) {
-          loadedData.push({
-            id: key,
-            subject: resData[key].subject,
-            description: resData[key].description,
-            recipitents: resData[key].recipients,
-            isDeleted: resData[key].isDeleted,
-            seen: resData[key].seen,
-            from: resData[key].EmailReplace,
-            to: resData[key].recipients.split('@')[0],
-          });
-          // }
-        }
-        dispatch(SendActions.updateData(loadedData));
-        // setSentData(loadedData);
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    dispatch(sendBoxData());
+  };
+  const trashHandler = async (item) => {
+    dispatch(sentBoxTrash(item.id));
   };
   useEffect(() => {
     fetchData();
-  }, [sentDataReducer]);
-  const trashHandler = async (item) => {
-    const delet = await fetch(
-      `https://mail-box-client-1328c-default-rtdb.firebaseio.com/sentBox/${email}/${item.id}.json`,
-      {
-        method: 'PATCH',
-        body: JSON.stringify({
-          isDeleted: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  };
+  }, []);
   return (
     <React.Fragment>
-      <div className=" flex items-center justify-between border-b border-gray-700 border-opacity-30 ps-2 hover:shadow-lg">
-        <div className="ms-2 flex h-14 w-2/12 items-center justify-evenly  ">
-          <div className="flex  ">
-            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
-              <RxBox className="text-xl " />
-            </button>
-            <button className="rounded-lg px-1 py-2  text-gray-600 hover:bg-slate-500 hover:bg-opacity-30">
-              <TiArrowSortedDown className="text-md text-gray-600" />
-            </button>
-          </div>
-          <button className="rounded-full px-3 py-3 text-gray-600  hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
-            <TbRotate />
-          </button>
-          <button className="rounded-full px-3 py-3 text-gray-600  hover:bg-slate-500 hover:bg-opacity-30 hover:duration-700">
-            <TbDotsVertical />
-          </button>
-        </div>
-        <div className="flex h-12 items-center gap-x-2  text-gray-500">
-          <button className=" rounded-md px-1 py-2 text-sm  hover:bg-slate-500 hover:bg-opacity-30">
-            1-10 of 50
-          </button>
-          <div className="flex gap-x-4">
-            <button className="rounded-full px-3 py-3 duration-700  hover:bg-slate-500 hover:bg-opacity-30">
-              <LiaAngleLeftSolid className=" text-md" />
-            </button>
-            <button className="rounded-full px-3 py-3 duration-700  hover:bg-slate-500 hover:bg-opacity-30">
-              <LiaAngleRightSolid className="text-md" />
-            </button>
-          </div>
-          <div className="me-4 flex">
-            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
-              <BsKeyboardFill className=" text-md  " />
-            </button>
-            <button className="rounded-lg px-1 py-2  hover:bg-slate-500 hover:bg-opacity-30">
-              <TiArrowSortedDown className=" text-md  " />
-            </button>
-          </div>
-        </div>
-      </div>
-      <ul className="">
+      <ContentHeder />
+      <ul>
         {sentDataReducer.map((item) => {
           if (!item.isDeleted) {
             return (
@@ -127,7 +48,7 @@ const Sent = () => {
                     <p>{item.description}</p>
                   </div>
 
-                  <button className=" invisible me-4 flex items-center gap-x-4 text-lg hover:text-gray-900 group-hover:visible">
+                  <button className="  me-4 flex items-center gap-x-4 text-lg hover:text-gray-900 group-hover:visible">
                     <BiArchiveIn />
                     <TbTrash
                       onClick={(event) => {
